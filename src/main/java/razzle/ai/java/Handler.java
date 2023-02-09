fbeada55fe69f1b14769741974aa697a62a22a8b
Julian Duru <durutheguru@gmail.com>
2023-02-09 10:09:59 +0100

    - Completed Handler registration

--- src/main/java/razzle/ai/java/Handler.java
+++ src/main/java/razzle/ai/java/Handler.java
@@ -1,6 +1,9 @@
 package razzle.ai.java;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.lang.reflect.Method;
 
@@ -8,13 +11,26 @@ import java.lang.reflect.Method;
  * created by julian on 09/02/2023
  */
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class Handler {
 
 
-    private Object bean;
+    private String name;
+
+
+    private String description;
+
+
+    private String[] roles;
 
 
     private Method method;
 
 
+    private Object bean;
+
+
 }
+
